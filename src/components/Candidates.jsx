0b510120194146049bc8dd0644a090a354d7a489
KilHyun-Kim <rlaarlfgus@gmail.com>
2020-03-07 16:33:54 +0900

    AWS의 S3를 사용하여 React SPA를 테스트로 업로드 하기위한 준비를 마쳤다.

--- src/components/Candidates.jsx
+++ src/components/Candidates.jsx
@@ -4,18 +4,17 @@ const Candidates = props => {
   return (
     <>
       <div className="candidatesWrap">
-        <div className="candidatesImage item">
-          <div>
+        <div className="candidatesImage">
+          <div className="__hooboImage">
             <img src={props.src} key={props.index} />
           </div>
           <div className="__kiho">
             <h2>
               기호 {props.kiho}번 {props.name}
             </h2>
-            <h3>{props.dang}</h3>
           </div>
         </div>
-        <div className="__gongyark item">
+        <div className="__gongyark">
           <div className="__introduce">
             <h2>{props.introduce}</h2>
           </div>
@@ -26,9 +25,11 @@ const Candidates = props => {
               <li>(3) {props.desc3}</li>
             </ul>
           </div>
+          <div className="dangImg">
+            <img src={props.dangImg} />
+          </div>
         </div>
       </div>
-      ;
     </>
   );
 };
--- src/components/Notice.jsx
+++ src/components/Notice.jsx
@@ -12,12 +12,31 @@ const Notice = () => {
         <GoAlert size={20} color="blue" />
       </p>
       <ul>
+        <li className="fontEmphasize">
+          현재 개발 중인 사이트입니다.
+          <br /> 테스트용 입니다
+        </li>
+        <li className="fontEmphasize">
+          <hr />
+          지도에서 부산을 클릭후 기장군으로 들어가주세요!
+        </li>
+        <li className="fontEmphasize">
+          <hr />
+          이 사이트를 보고 피드백을 주세요!
+          <br />
+          직접 연락을 해주시거나
+          <br />
+          rlaarlfgus@gmail.com 으로
+          <br />
+          피드백 주시면 감사하겠습니다!
+        </li>
+
         <li>
           이 사이트의 목적은 2020.4.15에 시행하는 제 21대 대한민국 국회의원
           선거의 각 지역 후보들의 정보와 공약을 선거권을 가진 사람들이 손쉽게
           접근할 수 있기 위해 만든 웹 사이트 입니다.
         </li>
-        <li>
+        {/* <li>
           "이 사이트는 <span>절대</span> 상업적으로 사용하지 않습니다."
         </li>
         <li>
@@ -29,8 +48,8 @@ const Notice = () => {
         <li>
           "이 사이트는 <span>절대</span> 위에 쓴 목적과는 다르게 쓰이지
           않습니다."
-        </li>
-        <li className="fontEmphasize">"Made by KiliKili"</li>
+        </li> */}
+        {/* <li className="fontEmphasize">"Made by KiliKili"</li> */}
       </ul>
     </div>
   );
