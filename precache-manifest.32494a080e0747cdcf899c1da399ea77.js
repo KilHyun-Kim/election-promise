e4c96af6cabda10b62557d1b7cec65a273f562a0
KilHyun-Kim <rlaarlfgus@gmail.com>
2020-08-24 15:13:04 +0900

    Updates

---
+++ precache-manifest.32494a080e0747cdcf899c1da399ea77.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90c2aab7bb97a1af4ef8af0babfed473",
+    "url": "/election-promise/index.html"
+  },
+  {
+    "revision": "27d729273c6e51bab7d4",
+    "url": "/election-promise/static/css/main.2ae871b6.chunk.css"
+  },
+  {
+    "revision": "6568b04ef53a9f23a1d7",
+    "url": "/election-promise/static/js/2.33a57dd0.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/election-promise/static/js/2.33a57dd0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27d729273c6e51bab7d4",
+    "url": "/election-promise/static/js/main.7ab088a3.chunk.js"
+  },
+  {
+    "revision": "2921b07e798569a461c0",
+    "url": "/election-promise/static/js/runtime-main.471ff2e2.js"
+  },
+  {
+    "revision": "e7f8197106f4b40e3062dc556272d71f",
+    "url": "/election-promise/static/media/Busan (1).e7f81971.jpg"
+  },
+  {
+    "revision": "b84515ac43769eebe6c024c74829d8db",
+    "url": "/election-promise/static/media/Chungcheongbukdo.b84515ac.jpg"
+  },
+  {
+    "revision": "2bc61a6210b38c057aae4fa3132db63f",
+    "url": "/election-promise/static/media/Chungcheongnamdo.2bc61a62.jpg"
+  },
+  {
+    "revision": "9bae13fafb112e99c629adc062e04258",
+    "url": "/election-promise/static/media/Daegu.9bae13fa.png"
+  },
+  {
+    "revision": "cda81b0e55356269b2e8aa1ab8a93f9a",
+    "url": "/election-promise/static/media/Daejeon.cda81b0e.gif"
+  },
+  {
+    "revision": "2f0ab8786887a422ce1e2e7f0d9de180",
+    "url": "/election-promise/static/media/Gangwondo.2f0ab878.png"
+  },
+  {
+    "revision": "50e1d9bb61d3c37bf15db1ea409c188d",
+    "url": "/election-promise/static/media/Gwangju.50e1d9bb.gif"
+  },
+  {
+    "revision": "0ac2f3c9c39d081601fc0b9b6de70f73",
+    "url": "/election-promise/static/media/Gyeongsangbukdo.0ac2f3c9.gif"
+  },
+  {
+    "revision": "3c16fb51976ad3a153ce9dc786ffad50",
+    "url": "/election-promise/static/media/Gyeongsangnamdo.3c16fb51.jpg"
+  },
+  {
+    "revision": "63f9115f457234cd1c5bc129aef6427c",
+    "url": "/election-promise/static/media/Incheon.63f9115f.gif"
+  },
+  {
+    "revision": "badbb0ea128524788211871f9fb3001c",
+    "url": "/election-promise/static/media/Jeju.badbb0ea.gif"
+  },
+  {
+    "revision": "0aa8892bf75d9847210d9a1db5a228fa",
+    "url": "/election-promise/static/media/Jeollabukdo.0aa8892b.jpg"
+  },
+  {
+    "revision": "80079dc2c8af19d4bb71786352fc9b90",
+    "url": "/election-promise/static/media/Jeollanamdo.80079dc2.jpg"
+  },
+  {
+    "revision": "c30f9f383e14fece445b7887d888bdf2",
+    "url": "/election-promise/static/media/Ulsan.c30f9f38.jpg"
+  },
+  {
+    "revision": "2fc180f052d01b9fb7db309363601c44",
+    "url": "/election-promise/static/media/chungju.2fc180f0.png"
+  },
+  {
+    "revision": "f31fd3c031df8e5e6777575de4bbd6ca",
+    "url": "/election-promise/static/media/koreaColormap.f31fd3c0.gif"
+  },
+  {
+    "revision": "8845a431022c53ba68813a68d3a78572",
+    "url": "/election-promise/static/media/경기도.8845a431.png"
+  }
+]);
